5e7327e42f33bc53ab8ee99286d1a8d2b3869c2f
iProsisTomer <47002031+iProsisTomer@users.noreply.github.com>
2019-11-20 12:45:07 +0200

    Add files via upload

--- ticket.js
+++ ticket.js
@@ -409,11 +409,12 @@ Boarding Pass
 </symbol>
 </svg>
       
-      <script> https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js
+     
         
-       
-      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js">
       </script>
+      
+   
   
 	`;
 
